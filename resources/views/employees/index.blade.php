e71efb098f55290bdc64686ae0ca66025806ab4f
Farah H <macriv@Farah-H.local>
2025-10-01 09:57:19 +0700

    Penambahan Master Layout

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -8,6 +8,10 @@
 </head>
 
 <body>
+    @extends('master')
+    @section('title', 'Daftar Pegawai')
+    @section('content')
+
     <div class="container mt-5">
         <h1 class="mb-4">Daftar Pegawai</h1>
         <table border="1" cellpadding="5" cellspacing="0">
@@ -47,6 +51,7 @@
             </tbody>
         </table>
     </div>
+    @endsection
 </body>
 
 </html>
